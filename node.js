53844496984e51093abc982eb28d407667ccfb29
DeekshithVemula <48466367+DeekshithVemula@users.noreply.github.com>
2019-10-09 18:11:46 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+var http=require('http');
+
+var Request=require('request');
+
+var port = 80;
+
+
+var host =process.env.HOST;
+
+var s = http.createServer();
+
+console.log("Hello world");
+
+s.listen(port);
